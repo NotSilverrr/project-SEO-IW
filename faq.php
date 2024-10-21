188d7d568bc60bca8f5ecd0f8b533f4f51330504
narvalo8 <mathis.latimier@gmail.com>
2024-10-21 17:10:24 +0200

    test

---
+++ faq.php
@@ -0,0 +1,15 @@
+<?php include_once 'includes/head.php'?>
+<body>
+<?php include_once 'includes/header.php'?>
+    <main class="container">
+        <h1>FAQ sur Gigaboulet – Tout savoir sur l'achat de cailloux chez Gigaboulet</h1>
+        <section>
+            <h2>Quels types de cailloux proposez-vous ?</h2>
+            <p>Nous proposons une large gamme de cailloux, de toutes les tailles et de toutes les couleurs. Vous trouverez forcément le caillou qu'il vous faut chez Gigaboulet !</p>
+        </section>
+        
+    </main>
+<?php include_once 'includes/footer.php'?>
+
+</body>
+</html>
