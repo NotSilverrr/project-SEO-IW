1c40bc40943ad387aba54f25b82027406634fd5b
NotSilverrr <tronsin7@gmail.com>
2024-10-21 16:14:02 +0200

    base structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GigaBoulet</title>
+</head>
+<body>
+    <?php include("includes/header.php");?>
+    <main>
+        <h1>GigaBoulet</h1>
+    </main>
+    <?php include("includes/footer.php");?>
+</body>
+</html>
