aa409a375f8eea96f596c636b9c085c350b24826
NotSilverrr <tronsin7@gmail.com>
2024-10-21 16:43:27 +0200

    add include head + header css start

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,8 @@
-<header></header>
+<header>
+    <a href="index.html"><img src="static/img/gigaboulet.png" class="header_logo"></a>
+    <ul class="header_list">
+        <li>Magasin</li>
+        <li>Notre histoire</li>
+        <li>Q & A</li>
+    </ul>
+</header>
--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="static/css/style.css">
-    <title>GigaBoulet</title>
-</head>
+<?php include("includes/head.php");?>
 <body>
     <?php include("includes/header.php");?>
     <main>
