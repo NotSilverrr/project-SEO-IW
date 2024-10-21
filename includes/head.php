8afe4a2e3d8565db8c8a0dd5a038bbadba40f341
NotSilverrr <tronsin7@gmail.com>
2024-10-21 18:13:46 +0200

    add story + mainpage seo improvement

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Gigaboulet - Découvrez la beauté des pierres naturelles. Boutique de cailloux décoratifs et fonctionnels pour décorer, créer un espace zen, ou réaliser des projets artistiques.">
     <link rel="stylesheet" href="static/css/style.css">
     <title>GigaBoulet</title>
 </head>
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,7 @@
 <header>
-    <a href="index.php"><img src="static/img/gigaboulet.png" class="header_logo"></a>
+    <a href="index.php"><img src="static/img/gigaboulet.png" class="header_logo" alt="logo"></a>
     <ul class="header_list">
-        <li>Magasin</li>
-        <li>Notre histoire</li>
+        <li><a href="story.php">Notre histoire</a></li>
         <li><a href="./../faq.php">Faq</a></li>
     </ul>
 </header>
