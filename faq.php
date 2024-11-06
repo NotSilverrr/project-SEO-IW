19a705b10f4e689bd82e55d0106abc349a14e758
Mathis Latimier <mathis.latimier@gmail.com>
2024-11-06 23:09:10 +0100

    commit

--- faq.php
+++ faq.php
@@ -10,6 +10,15 @@ include_once 'includes/head.php'?>
             <h2>Quels types de cailloux proposez-vous ?</h2>
             <p>Nous proposons une large gamme de cailloux, de toutes les tailles et de toutes les couleurs. Vous trouverez forcément le caillou qu'il vous faut chez <strong>Gigaboulet</strong> !</p>
         </section>
+        <section>
+            <h2>Comment choisir la taille de son Gigaboulet ?</h2>
+            <p>La taille du Gigaboulet dépend de l'usage que vous souhaitez en faire. Pour décorer votre jardin, un Gigaboulet de taille moyenne sera parfait. Pour un usage en intérieur, un petit Gigaboulet sera plus adapté.</p>
+        </section>
+        <section>
+            <h2>Quels sont les délais de livraison ?</h2>
+            <p>Les délais de livraison varient en fonction de la taille de votre commande. Pour une commande de moins de 10 Gigaboulet, la livraison est garantie en moins de 48h. Pour une commande plus importante, comptez entre 3 et 5 jours ouvrés.</p>
+        </section>
+
         
     </main>
 <?php include_once 'includes/footer.php'?>
