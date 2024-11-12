00879805072121cd6c7eb810c7596108b3cc80fd
Thomas R <tronsin7@gmail.com>
2024-11-12 21:14:45 +0100

    update story.php

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <header>
     <div class="container">
-        <a href="index.php"><img src="static/img/gigaboulet.png" class="header_logo"></a>
+        <a href="index.php"><img src="static/img/gigaboulet.png" class="header_logo" alt="Logo_Gigaboulet"></a>
         <nav>
             <ul class="header_list">
                 <li><a href="">Magasin</a></li>
-                <li><a href="">Notre histoire</a></li>
+                <li><a href="./../story.php">Notre histoire</a></li>
                 <li><a href="./../faq.php">Faq</a></li>
             </ul>
         </nav>
