1a5fd4dd833a23177dffcc9ac88bac7973a491c1
Julien <julienlemoing94@gmail.com>
2024-11-12 22:57:21 +0100

    Ajout dimage 2.0

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
         <a href="index.php"><img src="static/img/gigaboulet.webp" class="header_logo" alt="Logo_Gigaboulet"></a>
         <nav>
             <ul class="header_list">
-                <li><a href="">Magasin</a></li>
+                <li><a href="./../market.php">Magasin</a></li>
                 <li><a href="./../story.php">Notre histoire</a></li>
                 <li><a href="./../why.php">Pourquoi Nous ?</a></li>
                 <li><a href="./../faq.php">Faq</a></li>
