535878fa0152140d6673ffee8d70db20bf90be0e
Thomas R <tronsin7@gmail.com>
2024-11-12 21:35:16 +0100

    add strong gigaboulet + why.php

--- story.php
+++ story.php
@@ -8,13 +8,13 @@ include_once 'includes/head.php';?>
             Tout a commencé avec une passion simple mais puissante : les cailloux. Ceux que l'on trouve sur les plages, dans les montagnes, ou même au bord des chemins. Ces pierres qui racontent des histoires millénaires, forgées par la nature, polies par le temps, chacune avec sa forme unique et ses couleurs variées.
         </div>
         <div class="container story-p">
-            Gigaboulet est né de l'amour pour ces merveilles naturelles, et du rêve de les partager avec le monde. Nous avons commencé comme de simples collectionneurs, ramassant des pierres lors de nos voyages, fascinés par leur grandeur. Nous avons vu des cailloux qui pouvaient décorer, apaiser, inspirer des idées, ou même être transformés en outils pratiques.
+        <strong>Gigaboulet</strong> est né de l'amour pour ces merveilles naturelles, et du rêve de les partager avec le monde. Nous avons commencé comme de simples collectionneurs, ramassant des pierres lors de nos voyages, fascinés par leur grandeur. Nous avons vu des cailloux qui pouvaient décorer, apaiser, inspirer des idées, ou même être transformés en outils pratiques.
         </div>
         <div class="container story-p">
-            Ainsi est né Gigaboulet, un site dédié à tous ceux qui, comme nous, voient la beauté et la valeur des pierres dans leur forme la plus naturelle. Que ce soit pour ajouter une touche d'élégance à un jardin, créer un espace zen chez soi, ou pour des projets artistiques et artisanaux, nous voulons offrir des cailloux qui inspirent. Des pierres choisies avec soin, qui répondent à tous les besoins : esthétiques, pratiques, ou spirituels.
+            Ainsi est né <strong>Gigaboulet</strong>, un site dédié à tous ceux qui, comme nous, voient la beauté et la valeur des pierres dans leur forme la plus naturelle. Que ce soit pour ajouter une touche d'élégance à un jardin, créer un espace zen chez soi, ou pour des projets artistiques et artisanaux, nous voulons offrir des cailloux qui inspirent. Des pierres choisies avec soin, qui répondent à tous les besoins : esthétiques, pratiques, ou spirituels.
         </div>
         <div class="container story-p">
-            Ce qui a commencé comme une simple passion est devenu une communauté de passionnés. Aujourd'hui, Gigaboulet est plus qu'un simple magasin : c'est une invitation à reconnecter avec la nature, à apprécier la beauté cachée sous nos pieds, et à voir la magie dans chaque petit boulet que nous rencontrons. Parce qu'au bout du compte, chaque pierre a une histoire à raconter, et nous sommes ici pour les partager avec vous.
+            Ce qui a commencé comme une simple passion est devenu une communauté de passionnés. Aujourd'hui, <strong>Gigaboulet</strong> est plus qu'un simple magasin : c'est une invitation à reconnecter avec la nature, à apprécier la beauté cachée sous nos pieds, et à voir la magie dans chaque petit boulet que nous rencontrons. Parce qu'au bout du compte, chaque pierre a une histoire à raconter, et nous sommes ici pour les partager avec vous.
         </div>
     </main>
 <?php include_once 'includes/footer.php'?>
---
+++ why.php
@@ -0,0 +1,39 @@
+<?php 
+$title = "Gigaboulet - Pourquoi nous ?";
+include_once 'includes/head.php';?>
+<body>
+<?php include_once 'includes/header.php'?>
+    <main class="container">
+    <div class="story-p">
+        <p>Chez <strong>Gigaboulet</strong>, nous croyons que les pierres ne sont pas seulement des éléments de la nature, mais aussi des sources de bien-être, d'inspiration et de force. Qu'il s'agisse de décorer un espace, de méditer pour un esprit apaisé, ou de créer des œuvres artistiques, les cailloux que nous proposons sont là pour embellir votre quotidien. Avec <strong>Gigaboulet</strong>, chaque pierre devient une solution pour alléger les tracas de la vie moderne et retrouver un lien avec la nature. Notre mission est simple : vous offrir les meilleurs cailloux pour répondre à tous vos besoins, grands et petits.</p>
+    </div>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Site</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href="https://www.direct-cailloux.com" target="_blank">direct-cailloux.com</a></td>
+                    <td>Notre principal fournisseur de cailloux,nous somme particulièrement fier de notre relation avec direct-cailloux</td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.decovrac.net/12-graviers" target="_blank">decovrac.net</a></td>
+                    <td>Ce partenariat nous permet de proposer tout type de gravier, quand il d'agis de decovrac nous pouvons etre sur que c'est de la bonne qualité</td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.rocshop.fr/nos-produits" target="_blank">rocshop.fr</a></td>
+                    <td>Rocshop est le tout nouveau membre de la famille <strong>Gigaboulet</strong>, c'est un fournisseur fiable et nos premier retour client sont excelent</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </main>
+<?php include_once 'includes/footer.php'?>
+
+</body>
+</html>
