972b200c5230e7b9df2f31c067f5f23fd94af104
Thomas R <tronsin7@gmail.com>
2024-11-20 10:46:53 +0100

    404 test

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php 
+$title = "Gigaboulet - 404";
+include_once 'includes/head.php';?>
+<body>
+<?php include_once 'includes/header.php'?>
+    <main class="container">
+        <h1>Erreur 404, espèce de <strong>Gigaboulet</strong> !</h1>
+
+    </main>
+<?php include_once 'includes/footer.php'?>
+
+</body>
+</html>
