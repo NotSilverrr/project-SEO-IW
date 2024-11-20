dfd268be4802d621f8f94d28838a807f2d71cea2
Thomas R <tronsin7@gmail.com>
2024-11-20 11:05:56 +0100

    add 404

--- 404.php
+++ 404.php
@@ -1,11 +1,12 @@
 <?php 
 $title = "Gigaboulet - 404";
-include_once 'includes/head.php';?>
+include_once 'includes/head.php';
+header("HTTP/1.0 404 Not Found");
+?>
 <body>
 <?php include_once 'includes/header.php'?>
-    <main class="container">
+    <main class="container centered">
         <h1>Erreur 404, espèce de <strong>Gigaboulet</strong> !</h1>
-
     </main>
 <?php include_once 'includes/footer.php'?>
 
