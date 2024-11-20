aeb978d1b634c184f14bd95c6a339c232390966d
Mathis Latimier <mathis.latimier@gmail.com>
2024-11-20 11:11:38 +0100

    ajout burger menu

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,23 @@
                 <li><a href="/pourquoi-nous">Pourquoi Nous ?</a></li>
                 <li><a href="/faq">Faq</a></li>
             </ul>
+            
         </nav>
+        <button class="burger-button" id="burger-button" onclick="openBurger()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
     </div>
+    <script src="../static/js/burger.js"></script>
 </header>
+
+<section class="burger-menu" id="burger-menu">
+    <button class="close" onclick="closeBurger()">X</button>
+    <ul class="burger_list">
+        <li><a href="./../market.php">Magasin</a></li>
+        <li><a href="./../story.php">Notre histoire</a></li>
+        <li><a href="./../why.php">Pourquoi Nous ?</a></li>
+        <li><a href="./../faq.php">Faq</a></li>
+    </ul>
+</section>
