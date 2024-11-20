944d81390ae9c561c209f32dc6c1aa0db226a568
Thomas R <tronsin7@gmail.com>
2024-11-20 14:13:26 +0100

    add content to head

--- includes/head.php
+++ includes/head.php
@@ -9,4 +9,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title><?= $title ?></title>
     <meta name="description" content="<?= $description?>">
+    <script type="application/ld+json">
+    {
+    "@context": "https://gigaboulet.shop",
+    "@type": "LocalBusiness",
+    "name": "Gigaboulet",
+    "image": "https://gigaboulet.shop/static/img/gigaboulet_rond.ico",
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "123 Rue des boulets",
+        "addressLocality": "Paris",
+        "addressRegion": "Ile-de-france",
+        "postalCode": "75011",
+        "addressCountry": "France"
+    },
+    "telephone": "0783899806",
+    "openingHours": "Mo-Fr 07:00-21:00"
+    }
+    </script>
 </head>
