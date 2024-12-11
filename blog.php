0bd4815d1aa627dfba0430ac5716ca986bead338
NotSilverrr <tronsin7@gmail.com>
2024-12-11 19:43:57 +0100

    update blogs

--- blog.php
+++ blog.php
@@ -7,7 +7,7 @@ include_once 'includes/head.php';?>
 <h1>Blog </h1>
 <h2>Nos articles longs : </h2>
 
-<a href="/gigaboulet-egypte" class="article-link">Les boulets égiptiens</a>
+<a href="/gigaboulet-egypte" class="article-link">Les boulets égyptiens</a>
 <a href="/gigaboulet-volcanique" class="article-link">Les boulets volcanique</a>
 <a href="/gigaboulet-sous-marin" class="article-link">Les boulets sous marins</a>
 
