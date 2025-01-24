e35daca8c614064ac6723465733b123dc5f04a33
NotSilverrr <tronsin7@gmail.com>
2025-01-24 12:10:11 +0100

    removed canonical

--- includes/head.php
+++ includes/head.php
@@ -11,9 +11,6 @@
 
     <link rel="preload" href="/static/img/gigaboulet_rond.ico" as="image">
 
-    <link rel="canonical" href="https://gigaboulet.shop">
-
-
     <link rel="stylesheet" href="/static/css/style.css">
     <link rel="icon" href="/static/img/gigaboulet_rond.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
